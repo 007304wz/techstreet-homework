ea66d58f07203475197affb6b33c6d1a73426096
007304wz <007304wz@gmail.com>
2017-08-19 19:26:56 -0400

    step 3, call LoginService from save() method

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularService } from '../angular/service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'login-component',
   templateUrl: './login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loggedIn:  string = null;
 
   constructor(
     private _service: AngularService,
-    private _builder: FormBuilder
+    private _builder: FormBuilder,
+    private _login: LoginService
   ) {
     this.loginForm = this._builder.group({
       username: '',
       password: ''
     });
   }
-
   ngOnInit() {
     this.loggedIn = localStorage.getItem('currentUser');
     this._service.setDirective(1);
   }
   save() {
     this._service.setDirective(2);
+    this._login.login();
   }
 }
